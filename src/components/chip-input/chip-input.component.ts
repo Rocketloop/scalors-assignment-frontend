6b86684d26e69a648a62d722a6c5fd0bed5258a5
S.F <me@snatch.org.ua>
2017-09-23 17:06:19 +0300

    Basic typeahead added html for suggestion rendering

--- src/components/chip-input/chip-input.component.ts
+++ src/components/chip-input/chip-input.component.ts
@@ -8,8 +8,16 @@ import {
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
 
 import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/concat';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/share';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
 
 enum Key {
     Back    = 8,
@@ -33,42 +41,44 @@ enum Key {
     }]
 })
 export class ChipInputComponent implements ControlValueAccessor, OnInit, OnDestroy {
-    @Input() public typeahead = Observable.of([]);
-    @ViewChild('suggestionList') suggestionList: ElementRef;
+    @Input() public typeahead: any[] = [];
+    @ViewChild('hiddenInput') public hiddenInput: ElementRef;
+    @ViewChild('suggestionTpl') public suggestionTpl: ElementRef;
 
-    public propagateChange = (_: any) => {};
+    public selectedItems: string[] = [];
+    public suggestionList: string[] = [];
+    public showSuggestions: boolean = false;
 
-    public selectedItems: string[] = [
-        'Go','Java', 'Dart',
-        'JavaScript', 'TypeScript',
-        'Perl', 'PHP', 'Anglar',
-        'Lisp', 'Clojure',
-        'Brainfuck', 'Haskel'
-    ];
+    private _onChange = (_: any) => {};
+    private subscrPool: Subscription[];
 
     constructor() {}
 
-    ngOnInit() {}
-
-    ngOnDestroy() {}
-
-    public removeItem(index: number) {
-        if (index > -1) {
-            this.selectedItems.splice(index, 1);
-        }
+    ngOnInit() {
+        //const keyDown = Observable.fromEvent(this.hiddenInput.nativeElement, 'keydown').publish();
+        const keyDown = this.onElementKeyDown();
+        this.subscrPool = [
+            this.findSuggestion(keyDown)
+        ];
     }
-    private _onChange = (_: any) => {};
+
+    ngOnDestroy() {
+        this.subscrPool.forEach( sub => sub.unsubscribe() );
+        this.subscrPool.length = 0;
+    }
 
     public writeValue (e: KeyboardEvent): void {
-        if (e.keyCode === Key.Back && this.suggestionList.nativeElement.value == '') {
+        /*
+        if (e.keyCode === Key.Back && this.hiddenInput.nativeElement.value == '') {
             this.removeItem(this.selectedItems.length - 1);
             this._onChange(this.selectedItems);
         }
-        if (e.keyCode === Key.Enter && this.suggestionList.nativeElement.value !== '') {
-            this.selectedItems.push(this.suggestionList.nativeElement.value);
-            this.suggestionList.nativeElement.value = '';
+        if (e.keyCode === Key.Enter && this.hiddenInput.nativeElement.value !== '') {
+            this.selectedItems.push(this.hiddenInput.nativeElement.value);
+            this.hiddenInput.nativeElement.value = '';
             this._onChange(this.selectedItems);
         }
+        */
     }
 
     public registerOnChange (fn: any ): void {
@@ -76,4 +86,38 @@ export class ChipInputComponent implements ControlValueAccessor, OnInit, OnDestr
     }
 
     public registerOnTouched (fn: any): void {}
+
+    public removeItem(index: number) {
+        if (index > -1) {
+            this.selectedItems.splice(index, 1);
+        }
+    }
+
+    onElementKeyDown() {
+        return Observable.fromEvent(this.hiddenInput.nativeElement, 'keyup').share();
+    }
+
+    public findSuggestion(keyObj: Observable<{}>) {
+        return keyObj.filter((e: any) => this.filterNonCharKey(e.keyCode))
+            .map((e: any) => e.target.value )
+            .debounceTime(350)
+            .concat()
+            .distinctUntilChanged()
+            .filter((q: string) => q.length > 0)
+            .switchMap((q: string) => this.findInArray(q))
+            .subscribe( (results: string[]) => {
+                console.log(results);
+                this.suggestionList = results;
+                this.showSuggestions = true;
+            });
+    }
+
+    private findInArray(query: string): Observable<string[]> {
+        let regexp = new RegExp(query, 'ig');
+        return this.typeahead.map(item => item.filter((el: string) => { return regexp.test(el); }));
+    }
+
+    private filterNonCharKey(keyCode: number): boolean {
+        return Object.keys(Key).map(k => Key[k]).every( kCode => kCode !== keyCode );
+    }
 }
