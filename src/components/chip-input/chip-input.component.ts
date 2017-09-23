f795effc9f06799195cf1c9f2c668ed27c087fb4
S.F <me@snatch.org.ua>
2017-09-23 13:05:22 +0300

    Added font-awesome //just to use nice 'x' UI element aligned Methods for remove/add elements in array Basic keyboard handling W/o typeahead

--- src/components/chip-input/chip-input.component.ts
+++ src/components/chip-input/chip-input.component.ts
@@ -1,10 +1,26 @@
 import {
     Component,
     Input,
-    forwardRef,
+    ViewChild,
+    forwardRef, ElementRef,
     OnInit, OnDestroy
 } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/observable/of';
+
+enum Key {
+    Back    = 8,
+    Tab     = 9,
+    Enter   = 13,
+    Esc     = 27,
+    Left    = 37,
+    Up      = 38,
+    Right   = 39,
+    Down    = 40
+}
 
 @Component({
     selector: 'chip-input',
@@ -18,6 +34,17 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 })
 export class ChipInputComponent implements ControlValueAccessor, OnInit, OnDestroy {
     @Input() public typeahead = Observable.of([]);
+    @ViewChild('suggestionList') suggestionList: ElementRef;
+
+    public propagateChange = (_: any) => {};
+
+    public selectedItems: string[] = [
+        'Go','Java', 'Dart',
+        'JavaScript', 'TypeScript',
+        'Perl', 'PHP', 'Anglar',
+        'Lisp', 'Clojure',
+        'Brainfuck', 'Haskel'
+    ];
 
     constructor() {}
 
@@ -25,10 +52,28 @@ export class ChipInputComponent implements ControlValueAccessor, OnInit, OnDestr
 
     ngOnDestroy() {}
 
-    public writeValue (value: any): void {}
+    public removeItem(index: number) {
+        if (index > -1) {
+            this.selectedItems.splice(index, 1);
+        }
+    }
+    private _onChange = (_: any) => {};
 
-    public registerOnChange (fn: any): void {}
+    public writeValue (e: KeyboardEvent): void {
+        if (e.keyCode === Key.Back && this.suggestionList.nativeElement.value == '') {
+            this.removeItem(this.selectedItems.length - 1);
+            this._onChange(this.selectedItems);
+        }
+        if (e.keyCode === Key.Enter && this.suggestionList.nativeElement.value !== '') {
+            this.selectedItems.push(this.suggestionList.nativeElement.value);
+            this.suggestionList.nativeElement.value = '';
+            this._onChange(this.selectedItems);
+        }
+    }
+
+    public registerOnChange (fn: any ): void {
+        this._onChange = fn;
+    }
 
     public registerOnTouched (fn: any): void {}
-    
 }
