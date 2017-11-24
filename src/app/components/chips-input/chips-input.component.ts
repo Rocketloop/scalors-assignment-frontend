935a368ebf1d496318e3e2000acab4ec418e85f3
Sergii Solonar <sergeysolonar@gmail.com>
2017-11-24 16:24:50 +0200

    added example component from material library

--- src/app/components/chips-input/chips-input.component.ts
+++ src/app/components/chips-input/chips-input.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatChipInputEvent} from '@angular/material';
+import {ENTER} from '@angular/cdk/keycodes';
 
+const COMMA = 188;
+
+/**
+ * @title Chips with input
+ */
 @Component({
   selector: 'app-chips-input',
   templateUrl: './chips-input.component.html',
   styleUrls: ['./chips-input.component.css']
 })
-export class ChipsInputComponent implements OnInit {
+export class ChipsInputComponent {
+  visible: boolean = true;
+  selectable: boolean = true;
+  removable: boolean = true;
+  addOnBlur: boolean = true;
 
-  constructor() { }
+  // Enter, comma
+  separatorKeysCodes = [ENTER, COMMA];
 
-  ngOnInit() {
+  fruits = [
+    { name: 'Lemon' },
+    { name: 'Lime' },
+    { name: 'Apple' },
+  ];
+
+
+  add(event: MatChipInputEvent): void {
+    let input = event.input;
+    let value = event.value;
+
+    // Add our person
+    if ((value || '').trim()) {
+      this.fruits.push({ name: value.trim() });
+    }
+
+    // Reset the input value
+    if (input) {
+      input.value = '';
+    }
   }
 
+  remove(fruit: any): void {
+    let index = this.fruits.indexOf(fruit);
+
+    if (index >= 0) {
+      this.fruits.splice(index, 1);
+    }
+  }
 }
+
+
